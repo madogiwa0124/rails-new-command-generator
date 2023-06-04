6beff00ba37bc4bed729c9145d5873f42eeceb00
madogiwa <17684091+madogiwa0124@users.noreply.github.com>
2023-06-04 16:42:07 +0900

    fixup! configgured lint and formatter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaVersion: "latest",
     project: "./tsconfig.json",
   },
   root: true,
