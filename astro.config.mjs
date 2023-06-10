0fd519abc4a04a2fca0ae3b97443d7e40cf6a5b2
madogiwa <17684091+madogiwa0124@users.noreply.github.com>
2023-06-10 17:04:46 +0900

    configured deploy github action.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  site: "https://madogiwa0124.github.io",
+  base: "/rails-new-command-generator",
 });
